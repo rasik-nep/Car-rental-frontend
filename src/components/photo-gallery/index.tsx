bdc1dd926516bf5a8cf927b87ca46b03cf3a4572
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-02-29 23:16:07 +0545

    fix: unwanted imports in galley

--- src/components/photo-gallery/index.tsx
+++ src/components/photo-gallery/index.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import { Gallery } from "react-grid-gallery";
-import { images, CustomImage } from "./images";
+import { images } from "./images";
 import "yet-another-react-lightbox/styles.css";
 
 export default function App() {
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(0);
 
-  const handleClick = (index: number, item: CustomImage) => {
+  const handleClick = (index: number) => {
     setCurrentImage(index)
     setViewerIsOpen(true)
   };
