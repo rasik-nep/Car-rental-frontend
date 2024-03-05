127d8761ad0a59fdbb80ccf23a281400e6715e3e
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-03-05 15:37:17 +0545

    feat: render-as-you-fetch implemented in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,43 @@ import PhotoGallery from "../components/photo-gallery";
 import Price from "../components/price";
 import Footer from "../components/layout/Footer";
 
+import { useState, useEffect } from "react";
+// get from database
+import app from "../config/firebase-info"
+import { getDatabase, ref, get } from "firebase/database"
+
+// using render-as-you-fetch
 export default function Home() {
+    const [data, setData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [heroTitle, setHeroTitle] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const db = getDatabase(app);
+        const dbRef = ref(db, 'data');
+        const snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+            const data: { testText1: string, testText2: string }[] = Object.values(snapshot.val());
+            console.log(data);
+            setData(data);
+            const firstItem = data[0]; // Assuming you want to set heroTitle based on the first item in the array
+            if (firstItem) {
+                setHeroTitle(firstItem.testText1);
+            }
+            setIsLoading(false);
+        } else {
+            console.log("No data available");
+        }
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    
     return (
         <div className="text-black" >
             <Navbar />
