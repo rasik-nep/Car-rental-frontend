ff44de6d53a38e172745426524a2fca55aaae7a8
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-04-21 22:17:19 +0545

    fix: design changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,9 +13,9 @@ export default function About({ aboutData }: AboutProps) {
     return (
         <section className="full-container text-black p-7 md:h-[90vh] bg-red-200" id="about">
             <div className="flex flex-col md:flex-row py-5 md:py-10 lg:py-20 md:px-5 lg:px-10">
-                <div className="md:w-[50%] content-center p-5 md:p-10">
+                <div className="md:w-[50%] content-center py-5 md:p-10">
                     <h2 className="text-3xl md:text-5xl lg:text-7xl">{aboutData.title}</h2>
-                    <p className="w-[70%] mt-10">{aboutData.description}</p>
+                    <p className="md:w-[70%] mt-5 md:mt-10">{aboutData.description}</p>
                 </div>
                 <div className="md:w-[50%]">
                     <img
